c233c0b65ac9e6a66967d5298e45adc2bcc3d8a4
nullmatawasoradesu <119107850+wakaree@users.noreply.github.com>
2023-09-28 15:59:27 +0300

    Some improvements

--- bot/enums/locale.py
+++ bot/enums/locale.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from contextlib import suppress
 from enum import StrEnum, auto
 from typing import Optional
 
@@ -14,8 +13,6 @@ class Locale(StrEnum):
 
     @classmethod
     def resolve(cls, locale: Optional[str] = None) -> Locale:
-        if locale is None:
+        if locale not in cls.__members__:
             return Locale.DEFAULT
-        with suppress(ValueError):
-            return Locale(locale)
-        return Locale.DEFAULT
+        return Locale(locale)
--- bot/filters/magic_data.py
+++ bot/filters/magic_data.py
@@ -6,6 +6,10 @@ from magic_filter import MagicFilter
 
 class MagicData(_MagicData):
     def __init__(self, magic_data: MagicFilter | Any) -> None:
+        """
+        Cause PyCharm complains about an expression like F.smth == F.smth2,
+        thinking that it will return bool
+        """
         if not isinstance(magic_data, MagicFilter):
             raise TypeError(f"Expected MagicFilter got '{type(magic_data).__name__}'")
         super().__init__(magic_data=magic_data)
