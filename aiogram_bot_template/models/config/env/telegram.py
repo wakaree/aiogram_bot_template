fdbd6e598053ad215891541d6f2faa104090109d
Lowton05 <poka.tak.45@gmail.com>
2024-10-30 02:20:07 +0100

    Added new variable with admin ids in .env

--- aiogram_bot_template/models/config/env/telegram.py
+++ aiogram_bot_template/models/config/env/telegram.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from pydantic import SecretStr, field_validator
 
 from .base import EnvSettings
@@ -6,19 +8,34 @@ from .base import EnvSettings
 class TelegramConfig(EnvSettings, env_prefix="TELEGRAM_"):
     bot_token: SecretStr
     locales: str | list[str]
+    admin_ids: int | list[int]
     drop_pending_updates: bool
     use_webhook: bool
     reset_webhook: bool
     webhook_path: str
     webhook_secret: SecretStr
 
-    @field_validator("locales", mode="before")
-    def split_locales(cls, value: str | list[str]) -> list[str]:  # noqa: N805
+    @field_validator("locales", "admin_ids", mode="before")
+    def split_and_convert(cls, value: Any, info) -> list[str] | list[int]:  # type: ignore
         if isinstance(value, str):
             split_values = [item.strip() for item in value.split(",") if item]
-            return split_values
+        elif isinstance(value, int) and info.field_name == "admin_ids":
+            return [value]
         elif isinstance(value, list):
-            return value
-        raise ValueError(
-            f"Invalid format for TELEGRAM_LOCALES. Value received: {value} (type: {type(value)})"
-        )
+            split_values = value
+        else:
+            raise ValueError(
+                f"Invalid format for {info.field_name.upper()}. "
+                f"Value received: {value} (type: {type(value)})"
+            )
+
+        if info.field_name == "admin_ids":
+            try:
+                split_values_int = [int(item) for item in split_values]
+                return split_values_int
+            except ValueError:
+                raise ValueError(
+                    f"Invalid format for TELEGRAM_ADMIN_IDS. All values must be integers."
+                )
+
+        return split_values
